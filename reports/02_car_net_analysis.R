ba2643d571aa09ed4b41122835e116851e966013
Cecilie Svenningsen <cecilie.svenningsen@gmail.com>
2020-09-11 08:40:38 +0200

    started data analysis, subsetting data

--- reports/02_car_net_analysis.R
+++ reports/02_car_net_analysis.R
@@ -1,3 +1,19 @@
 # car net analysis
 
 ### load libraries ################
+library(tidyverse)
+library(readr)
+
+### load data #####################
+asvs <- read.delim("cleaned-data/asvtable.txt",sep=" ")
+data <- read.delim("cleaned-data/data.txt",sep=" ")
+taxonomy <- read.delim("cleaned-data/taxonomytable.txt",sep=" ")
+
+### subsetting the asv table ###################
+names(asvs) = gsub(pattern = "X*", replacement = "", x = names(asvs)) # remove the Xs that have emerged in the column headers that are numerical
+asvs <- asvs[, grepl("_QS$", names(asvs))] # only keep qiasymphony (automatic) purified samples
+names(asvs) <-  gsub(pattern = "_QS*", replacement = "", x = names(asvs)) # remove purification method from the data
+names(asvs)
+
+keep <- data$PCRID
+asvs <- asvs[, (names(asvs) %in% keep)] # subset asvtable to only contain samples with metadata
